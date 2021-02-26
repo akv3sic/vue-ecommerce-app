71c9af8ec9dbbe6d4c7bd8ed572ff9ee3da1742a
antekvesic <57301167+antekvesic@users.noreply.github.com>
2021-02-26 23:45:45 +0100

    redesign header and logo
    Add some extra padding. Redesign logo and nav links. Add tooltip to login button.

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,24 +1,33 @@
 <template>
         <div class="header no-print">
-            <v-app-bar app 
+            <v-app-bar
+                app  
                 color="white"
                 height="80"
-                elevate-on-scroll
+                :class="{'px-16':$vuetify.breakpoint.mdAndUp}"
             >
                 <!-- Mobile shopping basket button -->
                 <v-btn to="kosarica" class="hidden-md-and-up" text>
-                    <v-icon large left>mdi-basket-outline</v-icon>
+                    <v-icon left size="23">mdi-basket-outline</v-icon>
                     <v-badge color="accent" content="0"></v-badge>
                 </v-btn>
 
                 <!-- Logo area -->
                 <v-spacer class="hidden-md-and-up"></v-spacer>
-                 <img class="mr-1" :src="require('../../assets/logo.svg')" height="25"/>
+                
                 <v-toolbar-title class="text-uppercase">
+                    <v-row>
+                        <v-col justify-center>
                         <router-link to="/" class="text-decoration-none primary--text">
-                            <span class="font-weight-light">Dash</span>
-                            <span class="">Detailing</span>
+                            
+                            <span class="font-weight-normal primary--text d-none d-sm-inline">Dash</span>
+                            <img class="" :src="require('../../assets/logo.svg')" height="24"/>
+                            <span class="d-none d-sm-inline font-weight-bold">etailing</span>
                         </router-link>
+                        </v-col>
+                        
+                    </v-row>
+                        
                 </v-toolbar-title>
                 <v-spacer></v-spacer>
 
@@ -33,29 +42,31 @@
                 </span>
                  <!-- Standard menu -->
                 <v-toolbar-items class="hidden-sm-and-down">
-                    <v-btn to="/" text> 
-                        naslovna
-                    </v-btn>
-                      <v-btn to="/trgovina" text>
-                        trgovina
-                    </v-btn>
-                    <v-btn text to="/projekt">
-                        projekt
-                    </v-btn>
+                   <v-list-item
+                        v-for="(item, index) in navigationItems"
+                        :key="index"
+                        :to="item.to"
+                        active-class="no-active"
+                        class="on-hover mx-4"
+                        >
+                        <v-list-item-title class="text-body font-weight-medium">{{ item.title }}.</v-list-item-title>
+                        </v-list-item>
+                    
                 </v-toolbar-items>
+        
                 <v-spacer class="hidden-sm-and-down"></v-spacer>
-                
-                <v-toolbar-items class="hidden-sm-and-down">
 
+                <v-toolbar-items class="hidden-sm-and-down">
+                
                 <!-- Auth butttons -->
 
                  <!-- ******* Ako je korisnik prijavljen  POČETAK******* -->
                   <!-- Button with dropdown menu -->
-                <v-menu offset-y v-if="isLoggedIn">
+                <v-menu offset-y v-if="isLoggedIn" rounded="0" open-on-hover>
                     <template v-slot:activator="{ on, attrs }">    
                         <v-btn v-if="isLoggedIn" class="remove-text-transform" text v-bind="attrs" v-on="on">
-                                <v-icon>mdi-account</v-icon>
-                                Mate Matić
+                                <v-icon class="mr-1">mdi-account</v-icon>
+                                Mate
                         </v-btn>
                     </template>
                       <v-list>
@@ -69,17 +80,22 @@
                     </v-list>
                 </v-menu>
                  <!-- ******* Ako je korisnik prijavljen  KRAJ******* -->
-
+                
                  <!-- ******* Ako korisnik  NIJE prijavljen  POČETAK******* -->
-                    <v-btn v-else to="/prijava" class="remove-text-transform" text>
-                        <v-icon>mdi-login</v-icon>
-                        Prijava
-                    </v-btn>
+                    <v-tooltip v-else bottom>
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-btn v-bind="attrs" v-on="on" to="/prijava" class="remove-text-transform" text>
+                            <v-icon>mdi-login</v-icon>
+                        </v-btn>
+                    </template>
+                    <span>Prijavite se</span>
+                    </v-tooltip>
+                  
                  <!-- ******* Ako korisnik  NIJE prijavljen  KRAJ******* -->
                 
 
                     <!-- Shopping basket buttton -->    
-                    <v-btn to="/kosarica" class="" text>
+                    <v-btn to="/kosarica" class="" text small>
                         <v-icon>mdi-basket-outline</v-icon>
                         <v-badge color="accent" content="0"></v-badge>
                     </v-btn>
@@ -147,10 +163,15 @@ export default {
     data() {
         return {
             drawer: null,
+            navigationItems: [
+                {title: 'naslovna', to:'/'},
+                {title: 'trgovina', to:'/trgovina'},
+                {title: 'projekt', to:'/projekt'},
+            ],
             racunDropDownMenuItems: [
             { title: 'Moj račun', onClick: this.otvoriMojRacun},
             { title: 'Odjava', onClick: this.logOut},  
-        ],
+            ],
 
         }
     },
@@ -170,6 +191,17 @@ export default {
      }
 }
 </script>
+
+<style>
+.v-list-item--active.no-active:before {
+  opacity: 0 !important;
+}
+
+.v-list-item.on-hover:hover:before {
+  background-color:#052949;
+  opacity: 0.1 !important;
+}
+</style>
 
 <style scoped>
 .remove-text-transform {
