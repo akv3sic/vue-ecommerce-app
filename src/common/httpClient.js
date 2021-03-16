478286070b37089680e2a4f4fca95be139014cba
antekvesic <57301167+antekvesic@users.noreply.github.com>
2021-03-16 12:00:19 +0100

    fix(auth): missing authorization header when reloading a page

--- src/common/httpClient.js
+++ src/common/httpClient.js
@@ -4,11 +4,14 @@ import VueAxios from "vue-axios";
 
 Vue.use(VueAxios, axios)
 
+let token = window.localStorage.getItem('token');
+
 const httpClient = Vue.axios.create({
     baseURL: process.env.VUE_APP_BASE_URL,
     headers: {
         // defaultni headeri ove instance (httpClient) organizirani "key": "value"
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "Authorization": token ? 'Bearer ' + token : ""
     }
 });
 
