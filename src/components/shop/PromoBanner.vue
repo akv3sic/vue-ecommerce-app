2176f434126f77ff4f28b1291a0ffec4e0d1f89d
antekvesic <57301167+antekvesic@users.noreply.github.com>
2021-04-10 11:05:14 +0200

    rearange promo banner and add additional card

--- src/components/shop/PromoBanner.vue
+++ src/components/shop/PromoBanner.vue
@@ -11,8 +11,8 @@
                 <v-img
                 :src="card.src"
                 class="white--text align-end"
-                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                height="200px"
+                gradient="to bottom, rgba(0,0,0,.3), rgba(0,0,0,.8)"
+                :height="card.height"
                 >
                 <v-card-title v-text="card.title"></v-card-title>
                 <v-card-actions>
@@ -37,8 +37,9 @@ export default {
     name: "PromoBanner",
     data: () => ({
       cards: [
-        { title: 'Profesionalne polirke', src: 'https://res.cloudinary.com/sonax-gmbh/image/fetch/c_fill,h_823,q_auto:best,w_1920/c_crop,h_823,q_auto:best,w_1646,x_232,y_0/ar_2:1,c_scale,h_280,q_auto:eco,w_560/dpr_2,f_auto/https://sonax.de/var/site/storage/images/5/6/9/1/41965-3-eng-ROW/Image20191104143740.png', flex: 6 },
-        { title: '3M proizvodi', src: 'https://images-na.ssl-images-amazon.com/images/I/717Gs4SySEL._AC_SL1500_.jpg', flex: 6 },
+        { title: '3M proizvodi', src: 'https://images-na.ssl-images-amazon.com/images/I/717Gs4SySEL._AC_SL1500_.jpg', flex: 12, height:'240px' },
+        { title: 'Profesionalne polirke', src: 'https://res.cloudinary.com/sonax-gmbh/image/fetch/c_fill,h_823,q_auto:best,w_1920/c_crop,h_823,q_auto:best,w_1646,x_232,y_0/ar_2:1,c_scale,h_280,q_auto:eco,w_560/dpr_2,f_auto/https://sonax.de/var/site/storage/images/5/6/9/1/41965-3-eng-ROW/Image20191104143740.png', flex: 5, height:'210px' },
+        { title: 'Njega motornog prostora', src: 'https://images.pexels.com/photos/3689314/pexels-photo-3689314.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', flex: 7, height:'210px' },
       ],
     }),
 }
