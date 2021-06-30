fc22663d492698ea3f8d73e52ff1af39d8e81d7f
antekvesic <57301167+antekvesic@users.noreply.github.com>
2021-06-30 20:09:52 +0200

    set production tip to false

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import vuetify from './plugins/vuetify';
 import store from './store'
 
-Vue.config.productionTip = true
+Vue.config.productionTip = false
 
 new Vue({
   router,
