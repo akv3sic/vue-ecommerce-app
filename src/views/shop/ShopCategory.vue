489428e3d4ee68a538c05157367ed956d0494a6f
antekvesic <57301167+antekvesic@users.noreply.github.com>
2021-07-01 10:36:44 +0200

    remove search bar from category view

--- src/views/shop/ShopCategory.vue
+++ src/views/shop/ShopCategory.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="pa-2" :class="{'pa-0':$vuetify.breakpoint.xs, 'px-8':$vuetify.breakpoint.sm,'px-10':$vuetify.breakpoint.md, 'px-16':$vuetify.breakpoint.lgAndUp}">
     <Header />
-    <SearchBar />
     <PromoBanner />
     <Page />
     </div>
@@ -9,14 +8,13 @@
 
 <script>
 import Header from "@/components/layout/TheHeader";
-import SearchBar from "@/components/shop/TheSearchBar"
 import PromoBanner from "@/components/shop/PromoBanner"
 import Page from "@/components/shop/productsPage/TheProductsPage"
 import store from "@/store";
 
 export default {
     name: "shopHome",
-    components: { Header, SearchBar, PromoBanner, Page },
+    components: { Header, PromoBanner, Page },
     data: () => ({
 
     }),
